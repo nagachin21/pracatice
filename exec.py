0dfbf549499e64e218a00ce3bb31c7edd041f8c7
wakayama <y.wakayama21.43@gmail.com>
2020-05-30 23:42:18 +0900

    add quick sort

--- exec.py
+++ exec.py
@@ -2,15 +2,24 @@ import sys, os
 sys.path.append('./')
 import numpy as np
 from search import linear＿search, sentinel_linear_search, binary_search
-from sort import bubble_sort
+from sort import bubble_sort, quick_sort, random_quick_sort
 
 
 A = np.random.randint(20, size=10)
 seq_A = np.arange(0,10,1)
 
-sorted_A = bubble_sort(A, 10)
-print(sorted_A)
+#sorted_A = bubble_sort(A, 10)
+#print(sorted_A)
 
 #sentinel_linear_search(A, 20, 10)
 #linear_search(A, 20, 100)
 #binary_search(seq_A, 10, 2)
+
+print(A)
+print("Sorted by quick sort")
+sorted_A = quick_sort(A, 1, 10)
+print(sorted_A)
+print("--------------------------------------")
+print("Sorted by random quick sort")
+random_sorted_A = random_quick_sort(A, 1, 10)
+print(random_sorted_A)
--- sort.py
+++ sort.py
@@ -13,3 +13,51 @@ def bubble_sort(A, n):
             print(A)
         m-=1
     return A
+
+
+def quick_sort(A, L, R):
+    if L < R:
+        p = L
+        k = L + 1
+        while k <= R:
+            if A[k-1] < A[L-1]:
+                tmp = A[(p + 1) -1 ]
+                A[(p + 1) -1] = A[k-1]
+                A[k-1] = tmp
+                p+=1
+            k+=1
+        tmp = A[L-1]
+        A[L-1] = A[p-1]
+        A[p-1] = tmp
+
+        print("L: " + str(A[L-1:p-1]) + " p: " + str(A[p-1]) + " R: " + str(A[(p+1)-1:R]))
+
+        quick_sort(A, L, p-1)
+        quick_sort(A, p+1, R)
+    return A
+
+
+def random_quick_sort(A, L, R):
+    if L < R:
+        r = np.random.randint(L, R)
+        swap = A[L-1]
+        A[L-1] = A[r]
+        A[r] = swap
+        p = L
+        k = L + 1
+        while k <= R:
+            if A[k-1] < A[L-1]:
+                tmp = A[(p + 1) -1 ]
+                A[(p + 1) -1] = A[k-1]
+                A[k-1] = tmp
+                p+=1
+            k+=1
+        tmp = A[L-1]
+        A[L-1] = A[p-1]
+        A[p-1] = tmp
+
+        print("L: " + str(A[L-1:p-1]) + " p: " + str(A[p-1]) + " R: " + str(A[(p+1)-1:R]))
+
+        random_quick_sort(A, L, p-1)
+        random_quick_sort(A, p+1, R)
+    return A
