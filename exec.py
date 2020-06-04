d02d28bee3aa767c4c31630e0ffa0dcdb84b46d9
wakayama <y.wakayama21.43@gmail.com>
2020-06-04 22:22:08 +0900

    add merge_sort

--- exec.py
+++ exec.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append('./')
 import numpy as np
 from search import linear＿search, sentinel_linear_search, binary_search
-from sort import bubble_sort, quick_sort, random_quick_sort
+from sort import bubble_sort, quick_sort, randomized_quick_sort, insertion_sort, merge_sort
 
 
 A = np.random.randint(20, size=10)
@@ -16,10 +16,16 @@ seq_A = np.arange(0,10,1)
 #binary_search(seq_A, 10, 2)
 
 print(A)
-print("Sorted by quick sort")
-sorted_A = quick_sort(A, 1, 10)
+#print("Sorted by quick-sort")
+#sorted_A = quick_sort(A, 1, 10)
+#print("Sorted by Insertion-sort")
+#sorted_A = insertion_sort(A,10)
+#print(sorted_A)
+#print("--------------------------------------")
+#print("Sorted by randomized-quick-sort")
+#random_sorted_A = randomized_quick_sort(A, 1, 10)
+#print(random_sorted_A)
+count = 1
+sorted_A = merge_sort(A, count)
+print("Sorted by merged-sort")
 print(sorted_A)
-print("--------------------------------------")
-print("Sorted by random quick sort")
-random_sorted_A = random_quick_sort(A, 1, 10)
-print(random_sorted_A)
--- sort.py
+++ sort.py
@@ -14,6 +14,17 @@ def bubble_sort(A, n):
         m-=1
     return A
 
+def insertion_sort(A, n):
+    for i in range(1, n):
+        tmp = A[i]
+        j = i
+        while tmp < A[j-1] and j > 0:
+            A[j] = A[j-1]
+            j-=1
+        A[j] = tmp
+    return A
+
+
 
 def quick_sort(A, L, R):
     if L < R:
@@ -37,7 +48,7 @@ def quick_sort(A, L, R):
     return A
 
 
-def random_quick_sort(A, L, R):
+def randomized_quick_sort(A, L, R):
     if L < R:
         r = np.random.randint(L, R)
         swap = A[L-1]
@@ -58,6 +69,44 @@ def random_quick_sort(A, L, R):
 
         print("L: " + str(A[L-1:p-1]) + " p: " + str(A[p-1]) + " R: " + str(A[(p+1)-1:R]))
 
-        random_quick_sort(A, L, p-1)
-        random_quick_sort(A, p+1, R)
+        randomized_quick_sort(A, L, p-1)
+        randomized_quick_sort(A, p+1, R)
+    return A
+
+
+def merge_sort(A, cnt):
+    #print("Merge Sort : " + str(cnt) + " 回目 >> ")
+
+    if len(A) > 1:
+        mid = len(A) // 2
+        cnt += 1
+        #print( "L: " + str(A[:mid]) + " R: " + str(A[mid:]) )
+        L = merge_sort(A[:mid], cnt)
+        R = merge_sort(A[mid:], cnt)
+
+        A = np.empty(0)
+        i = j = 0
+        #print( "ソート開始 >> L: " + str(L) + " R: " + str(R) )
+        while len(L) > i and len(R) > j:
+            if L[i] < R[j]:
+                A = np.append(A, L[i])
+                #print("LをAに追加" + str(A))
+                i += 1
+            else:
+                A = np.append(A, R[j])
+                #print("RをAに追加" + str(A))
+                j += 1
+
+        if len(L) > i:
+            #print("残りのLをAに追加")
+            A = np.append(A, L[i:])
+        elif len(R) > j:
+            #print("残りのRをAに追加")
+            A = np.append(A, R[j:])
+    else:
+        #print(A)
+        pass
+
+    #print("<< Merge Sort : " + str(cnt) + " 回目")
+    #print("終了 : " + str(A))
     return A
