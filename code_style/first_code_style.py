33457c18a407b2c524b6578ef6eb15278926e730
wakayama <y.wakayama21.43@gmail.com>
2020-06-21 12:40:47 +0900

    start code style lecture

---
+++ code_style/first_code_style.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == '__main__':
+    f = False
+    if not f:
+        print('you are fired')
+
+    is_code_style_good = True
+    if not is_code_style_good:
+        print('you are fired')
+    else :
+        print('you are not fired')
